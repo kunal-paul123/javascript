35836bc9e64b892ae0de5c9f2acdbe7c7cc76303
Kunal Paul <kp2346578@gmail.com>
2024-07-19 12:55:10 +0000

    Conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,30 @@
+let score = null
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1, false => 0
+
+let isLoggedIn = null
+let boolean = Boolean(isLoggedIn)
+console.log(typeof boolean);
+console.log(boolean);
+
+// 1 => true, 0 => flase
+// "" => false
+// "kunal" => true
+// null => false
+
+let someNumber = 55
+
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber);
+console.log(stringNumber);
+
+// 55 => 55
