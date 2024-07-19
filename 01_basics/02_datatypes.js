c7fef547a11e707b5c834abcf9a0105a9826b16b
Kunal Paul <kp2346578@gmail.com>
2024-07-19 12:54:27 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,17 @@
+let name = "kunal";
+let age = 21;
+let isLoggedIn = true;
+
+//number => 2 to power 53
+// biginit
+//string => ""
+// boolean => true/false
+// null => standalone value
+// undefined 
+// symbol => unique
+
+
+// object
+
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
