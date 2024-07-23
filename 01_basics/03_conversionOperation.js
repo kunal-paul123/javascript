20aa7831b42813332cc7693aac7ad87035f8e229
Kunal Paul <kp2346578@gmail.com>
2024-07-23 06:23:32 +0000

    conversion of datatypes

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -28,3 +28,16 @@ console.log(typeof stringNumber);
 console.log(stringNumber);
 
 // 55 => 55
+
+// *********operations*********
+
+let str1 = "hello"
+let str2 = " kunal"
+
+let str3 = str1 + str2
+console.log(str3);
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
